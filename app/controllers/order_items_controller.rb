f0143abf0175e4dc84c46a9674ac216d8cae2d2b
jk2424 <jkim2244@gmail.com>
2018-05-14 14:16:50 -0400

    cleaning up style scripts and adding rails-ujs

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,9 +2,25 @@ class OrderItemsController < ApplicationController
   def create
     @order = current_order
     @order.status = OrderStatus.in_progress.id
-    @order_item = @order.order_items.new(order_item_params)
-    @order.save
+    # we need a way to determine when is a collection of items
+    if params.key?("multiple_items")
+      products = JSON.parse(params[:products])
+      products.each do |product|
+        existing_item = @order.order_items.find_by_product_id(product["product_id"])
+        ot = existing_item ? existing_item : @order.order_items.new
+        ot.orderitems_quantity = product["orderitems_quantity"]
+        ot.product_id = product["product_id"]
+        ot.save!
+      end
+    else
+      @order_item = @order.order_items.new(order_item_params)
+      @order.save
+    end
     session[:order_id] = @order.id
+    respond_to do |format|
+      format.json { render json: { success: true } }
+      format.html { redirect_to :mycart }
+    end
   end
 
   def update
@@ -16,7 +32,7 @@ class OrderItemsController < ApplicationController
 
 
     # @order_item.update_attribute(:orderitems_quantity, order_item_params[:orderitems_quantity])
-    # redirect_to :mycart
+    redirect_to :mycart
   end
 
   def destroy
@@ -24,7 +40,17 @@ class OrderItemsController < ApplicationController
     @order_item = @order.order_items.find(params[:id])
     @order_item.destroy
     @order_items = @order.order_items
+    redirect_to cart_path
   end
+
+#   def destroy
+#   order = session['order']
+#   item = order['items'].find { |item| item['product_id'] == params[:id] }
+#   if item
+#     cart['items'].delete item
+#   end
+#   redirect_to cart_path
+# end
 
   private
 
