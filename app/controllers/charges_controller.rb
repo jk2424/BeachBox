ea242d28ff422556400673b64be05573117ffc3f
OlehZavodenko <oleh.zavodenko@me.com>
2018-05-15 16:17:51 -0400

    charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,31 +1,40 @@
 class ChargesController < ApplicationController
   def new
-    from  = "549 NW 28th St, Miami, FL"
-    to  = "1 Ocean Drive, Miami Beach, FL"
-    @quote = $client.quote(pickup_address: from, dropoff_address: to)
     @amount = 1000
 
-    # package = {
-    #             :manifest => "a box of kittens",
-    #             :pickup_name => "The Warehouse",
-    #             :pickup_address => "20 McAllister St, San Francisco, CA",
-    #             :pickup_phone_number => "555-555-5555",
-    #             :pickup_business_name => "Optional Pickup Business Name, Inc.",
-    #             :pickup_notes => "Optional note that this is Invoice #123",
-    #             :dropoff_name => "Alice",
-    #             :dropoff_address => "101 Market St, San Francisco, CA",
-    #             :dropoff_phone_number => "415-555-1234",
-    #             :dropoff_business_name => "Optional Dropoff Business Name, Inc.",
-    #             :dropoff_notes => "Optional note to ring the bell",
-    #             :quote_id => "dqt_K9LFfpSZCdAJsk"
-    #           }
-    #
-    #   @delivery = $client.create(package)
+
+    from  = "1 washington avenue, Miami Beach, FL"
+    to  = "1 Ocean Drive, Miami Beach, FL"
+    @quote = $client.quote(pickup_address: from, dropoff_address: to)
+    pp @quote
+    # from_lat = "37.778307"
+    # from_lon = "-122.413524"
+    # to_lat = "37.778307"
+    # to_lon = "-122.413524"
+    # @quote = $client.quote(pickup_latitude: from_lat, pickup_longitude: from_lon, dropoff_latitude: to_lat, dropoff_longitude: to_lon)
+
+    package = {
+                :manifest => "a box of kittens",
+                :pickup_name => "The Warehouse",
+                :pickup_address => "20 McAllister St, San Francisco, CA",
+                :pickup_phone_number => "415-555-1234",
+                # :pickup_business_name => "Optional Pickup Business Name, Inc.",
+                :pickup_notes => "Optional note that this is Invoice #123",
+                :dropoff_name => "Alice",
+                :dropoff_address => "101 Market St, San Francisco, CA",
+                :dropoff_phone_number => "415-555-1234",
+                # :dropoff_business_name => "Optional Dropoff Business Name, Inc.",
+                :dropoff_notes => "Optional note to ring the bell",
+                # :quote_id => @quote.id #"dqt_K9LFfpSZCdAJsk"
+              }
+
+      @delivery = $client.create(package)
+      pp @delivery
   end
 
   def create
     # Amount in cents
-    # @amount = 
+    # @amount =
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail]
