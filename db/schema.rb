d6d33225d47a6516e0cd5d63f7432ef97edfeb58
jk2424 <jkim2244@gmail.com>
2018-05-18 12:18:52 -0400

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_13_143530) do
+ActiveRecord::Schema.define(version: 2018_05_17_211540) do
 
   create_table "Orders", force: :cascade do |t|
     t.string "street_address"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2018_05_13_143530) do
     t.decimal "shipping"
     t.decimal "total"
     t.index ["user_id"], name: "index_Orders_on_user_id"
+  end
+
+  create_table "attributes", force: :cascade do |t|
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_attributes_on_product_id"
   end
 
   create_table "order_items", force: :cascade do |t|
