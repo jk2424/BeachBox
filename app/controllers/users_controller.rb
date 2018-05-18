1be5b37543dea7dd638d34fa73bc043d7918a52a
Stephani <stephani.oliveros@gmail.com>
2018-05-18 17:17:48 -0400

    fixed registration error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    puts user_params
     @user = User.new(user_params)
+    puts @user
     # store all emails in lowercase to avoid duplicates and case-sensitive login errors:
     @user.email.downcase!
     
     if @user.save
       # If user saves in the db successfully:
       flash[:notice] = "Account created successfully!"
-      redirect_to buildbox_path
+      redirect_to login_path
     else
       # If user fails model validation - probably a bad password or duplicate email:
       flash.now.alert = "Oops, couldn't create account. Please make sure you are using a valid email and password and try again."
@@ -23,10 +25,12 @@ class UsersController < ApplicationController
   end
   
   def account_profile
+    @user = current_user
   end
 
   def update
     @user = User.find(params[:id])
+    puts @user
     if @user.update(user_params) 
       redirect_to account_path
     else 
